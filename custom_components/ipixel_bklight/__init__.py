c8744f7685b4f184dce13e94393dd36ff070dfb2
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2025-12-31 10:22:38 +0100

    Update __init__.py

--- custom_components/ipixel_bklight/__init__.py
+++ custom_components/ipixel_bklight/__init__.py
@@ -8,13 +8,25 @@ PLATFORMS = ["switch"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.data[DOMAIN][entry.entry_id] = {
+        "address": entry.data["address"],
+        "client": None,
+        "lock": None,
+    }
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    data = hass.data[DOMAIN].get(entry.entry_id)
+
+    if data and data["client"]:
+        try:
+            await data["client"].disconnect()
+        except Exception:
+            pass
+
     await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
